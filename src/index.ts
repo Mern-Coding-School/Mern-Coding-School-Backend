4fa5063af5dfc7927aa85d89d3c05442d2766e70
Abhishek Kumar <abhishek9661342993@gmail.com>
2025-02-03 03:01:34 +0530

    Add configuration files for ESLint, Prettier, and Jest; set up Husky pre-commit hook

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express'
 
-const app: Application = express();
-const port: number = 5000;
+const app: Application = express()
+const port: number = 5000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
-});
+  res.send('Hello, World!')
+})
 
-app.use(express.json());
+app.use(express.json())
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
